e3c799bfb02b21a1ee19d96d5d1bc5472881c6a3
Farhan <farhanmahmoodny@gmail.com>
2019-02-04 21:53:17 -0500

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,27 @@
+class Ingredient
+  @@all = []
 
+attr_accessor :ingredient
+
+  def initialize(ingredient)
+    @ingredient = ingredient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.most_common_allergen
+    all_count = {}
+    Allergen.all.each do |allergy|
+      all_count[allergy.allergen] = 0
+    end
+    Allergen.all.each do |allergy|
+      all_count[allergy.allergen] += 1
+    end
+  all_count.max_by do |allergy, count|
+    count
+  end[0]
+  end
+end
---
+++ app/models/RecipeIngredient.rb
@@ -0,0 +1,22 @@
+class RecipeIngredient
+  @@all = []
+attr_accessor :ingredient, :recipe
+
+  def initialize(ingredient, recipe)
+    @ingredient = ingredient
+    @recipe = recipe
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  #
+  # def ingredient
+  #
+  # end
+  #
+  # def recipe
+  #
+  # end
+end
